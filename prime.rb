536f68ab5abbfe5051587518863b78812ad02dbe
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-17 03:48:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
+  end
+end
+  #Add  code here!
